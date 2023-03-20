61147581422f922fda9c83791181295e63942706
Liebranca <dantepaola25@gmail.com>
2023-03-20 17:01:15 -0300

    singl grammar layouts

--- Grammar/SinGL.pm
+++ Grammar/SinGL.pm
@@ -55,8 +55,19 @@ BEGIN {
 
   sub Frame_Vars($class) { return {
 
+    -vx_attrs => [],
+    -px_attrs => [],
+
+    -uniforms => [],
+    -samplers => [],
+
+    -ubos     => [],
+    -ssbos    => [],
+
+    -cmode    => $NULLSTR,
+
     %{Grammar->Frame_Vars()},
-    -passes => ['_ctx','_opz','_run'],
+    -passes=>['_ctx','_opz','_run'],
 
   }};
 
@@ -73,6 +84,23 @@ BEGIN {
 
     ),
 
+    nparens=>Lang::nonscap(q[\)],
+
+      iv    => 1,
+      mod   => '+',
+      sigws => 1,
+
+    ),
+
+    q[meta-tag-key]=>Lang::eiths(
+
+      [qw(author version)],
+
+      insens => 1,
+      bwrap  => 1,
+
+    ),
+
     prim=>qr{(
 
       void | bool | int | uint | float
@@ -82,11 +110,6 @@ BEGIN {
 
       | sampler([^\s]+)?
       | buffer
-
-    )}x,
-
-    spec=>qr{(
-      const | uniform | in | out | flat
 
     )}x,
 
@@ -118,7 +141,18 @@ BEGIN {
 
     )}x,
 
+    q[decl-size]=>qr{\[\d+\]}x,
+
   };
+
+# ---   *   ---   *   ---
+# base patterns
+
+  rule('~<any>');
+  rule('~<prim>');
+  rule('~<name>');
+  rule('~<term>');
+  rule('~<nterm>');
 
 # ---   *   ---   *   ---
 # meta ops
@@ -149,24 +183,78 @@ sub pe_anchor($self,$branch) {
 
 sub pe_anchor_ctx($self,$branch) {
 
-  my $type=$branch->leaf_value(0);
+  my $frame = $self->{frame};
+  my $type  = $branch->leaf_value(0);
 
   $branch->clear();
+  $frame->{-cmode}=$type;
   $branch->{value}=$type;
 
-  # get nodes up to next hierarchical
-  my @out=$branch->{parent}->match_until(
-    $branch,qr{^pe-anchor$}
+  $branch->pushlv(
+    $branch->match_up_to(qr{^pe-anchor$})
 
   );
 
-  # ^all remaining on fail
-  @out=$branch->{parent}->all_from(
-    $branch
+};
 
-  ) if ! @out;
+sub pe_anchor_opz($self,$branch) {
 
-  $branch->pushlv(@out);
+  my $type=$branch->{value};
+
+  $self->paste_px_attrs($branch)
+  if (uc $type) eq 'FRAG';
+
+};
+
+# ---   *   ---   *   ---
+# syncs frag in to vert out
+
+sub paste_px_attrs($self,$branch) {
+
+  my $frame = $self->{frame};
+  my $attrs = $frame->{-px_attrs};
+
+  my $i     = 0;
+
+  for my $attr(@$attrs) {
+
+    $branch->insert($i,'BLANK');
+
+    my $dst = \$branch->{leaves}->[$i++];
+    my $cpy = $attr->dup();
+
+    $$dst=$cpy;
+
+    $$dst->{value}  = 'in';
+    $$dst->{parent} = $branch;
+
+  };
+
+};
+
+# ---   *   ---   *   ---
+# more meta
+
+  rule('~<meta-tag-key>');
+
+  rule(q[
+    $<meta-tag>
+    &meta_tag
+
+    meta-tag-key nterm term
+
+  ]);
+
+# ---   *   ---   *   ---
+# ^post-parse
+
+sub meta_tag($self,$branch) {
+
+  my $st=$branch->bhash();
+
+  $branch->clear();
+  $branch->init($st->{nterm});
+  $branch->{value}=$st->{q[meta-tag-key]};
 
 };
 
@@ -197,11 +285,6 @@ sub include($self,$branch) {
 
 # ---   *   ---   *   ---
 # shader inputs
-
-  rule('~<prim>');
-  rule('~<name>');
-  rule('~<term>');
-  rule('~<nterm>');
 
   rule('%<uniform-key=uniform>');
   rule('~<io-key>');
@@ -237,15 +320,201 @@ sub io($self,$branch) {
 };
 
 # ---   *   ---   *   ---
+# converts ins and outs
 
-  rule('~<any>');
-  rule('|<meta> &clip lcom pe-anchor include');
+sub io_ctx($self,$branch) {
+
+  my $frame = $self->{frame};
+  my $cmode = $frame->{-cmode};
+
+  my $key   = $branch->{value};
+  my $o     = $branch->leaf_value(0);
+
+  # remember vertex shader inputs
+  $self->reg_vx_attrs($branch)
+  if $key eq 'in';
+
+  # convert vertex out to frag in
+  $self->reg_px_attrs($branch)
+
+  if $key eq 'out'
+  && (uc $cmode) eq 'VERT'
+  ;
+
+};
+
+# ---   *   ---   *   ---
+# registers vertex shader in
+# as vertex attributes
+
+sub reg_vx_attrs($self,$o) {
+
+  my $frame=$self->{frame};
+  my $attrs=$frame->{-vx_attrs};
+
+  push @$attrs,$o;
+
+};
+
+# ---   *   ---   *   ---
+# registers vertex shader out
+# as frag shader in
+
+sub reg_px_attrs($self,$o) {
+
+  my $frame=$self->{frame};
+  my $attrs=$frame->{-px_attrs};
+
+  push @$attrs,$o;
+
+};
+
+# ---   *   ---   *   ---
+# interface blocks
+
+  rule('%<layout-key=layout>');
+  rule('%<buffer-key=buffer>');
+
+  rule('%<beg-parens=\(>');
+  rule('%<end-parens=\)>');
+
+  rule('~<nparens>');
+
+  rule(q[
+
+    $<layout-args>
+    &tween_clip
+
+    beg-parens
+    nparens
+
+    end-parens
+
+  ]);
+
+  rule(q[
+
+    |<layout-type>
+    &cclip
+
+    uniform-key buffer-key
+
+  ]);
+
+  rule('%<beg-scope=\{>');
+  rule('%<end-scope=\}>');
+
+  rule('~<decl-size>');
+  rule('?<opt-decl-size> &clip decl-size');
+
+  rule('$<decl> name name opt-decl-size');
+  rule('+<block-elems> decl term');
+
+  rule(q[
+
+    $<block-body>
+    &tween_clip
+
+    beg-scope
+    block-elems
+
+    end-scope
+
+  ]);
+
+# ---   *   ---   *   ---
+# ^post-parse
+
+sub decl($self,$branch) {
+
+  my @order = qw(type name size);
+  my $lv    = $branch->{leaves};
+
+  my $o     = {};
+
+  for my $nd(@$lv) {
+
+    my $key    = shift @order;
+    $o->{$key} = $nd->leaf_value(0);
+
+  };
+
+  $o->{size}//='1';
+  $o->{size}=~ s{^\[|\]$}{}sxmg;
+
+  $branch->clear();
+  $branch->{value}=$o;
+
+};
+
+# ---   *   ---   *   ---
+# ^layouts continued II
+
+  rule(q[
+
+    $<layout>
+
+    layout-key layout-args
+    layout-type name
+
+    block-body
+
+    name
+
+  ]);
+
+# ---   *   ---   *   ---
+# ^post-parse
+
+sub layout($self,$branch) {
+
+  my $re       = qr{^name$};
+  my $blk_name = $branch->branch_in($re);
+
+  $blk_name->{value}='block-name';
+
+  my $ice_name = $branch->branch_in($re);
+
+  $ice_name->{value}='ice-name';
+
+  my $st   = $branch->bhash(0,0,0,0,1,0);
+
+  my $args = $st->{q[layout-args]};
+  my @args = split $COMMA_RE,$args;
+
+  my $o    = {
+
+    type     => $st->{q[layout-type]},
+    args     => \@args,
+
+    body     => $st->{q[block-body]},
+
+    blk_name => $st->{q[block-name]},
+    ice_name => $st->{q[ice-name]},
+
+  };
+
+  $branch->clear();
+  $branch->init($o);
+
+};
+
+# ---   *   ---   *   ---
+
+  rule(q[
+    |<meta>
+    &clip
+
+    lcom meta-tag
+    pe-anchor include
+
+  ]);
 
   rule(q[
     |<needs-term-list>
     &clip
 
-    io uniform
+    io uniform layout
 
   ]);
 
@@ -268,14 +537,29 @@ sub io($self,$branch) {
 
 my $prog=q[
 
-  $:VERT;>
+  VERSION   v0.00.1b;
+  AUTHOR    'IBN-3DILA';
+
+$:VERT;>
 
   #include <path/to/file>
+
   uniform mat4 n0;
   flat in uint n1;
 
-  $:FRAG;>
-  bye
+  out uint n2;
+
+  layout (std430) buffer blockn {
+    uint x[128];
+    uint y[128];
+
+  } icen;
+
+$:FRAG;>
+
+void main(void) {
+
+};
 
 ];
 
